070f7de1822cb3d13854a911ebca3fed29c7fab3
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-29 17:10:57 +0500

    29-12-2022

---
+++ 2211E2 - 29-12-2022/app.js
@@ -0,0 +1,29 @@
+// alert("Hello World")
+
+// document.write("Hello World")
+
+// console.log("Hello World")
+
+// var 
+// let
+// const
+
+// let age=23
+// const name="Sameer"
+// let weight=60.75
+// var phone_1=03002200000
+
+
+// let age=+prompt("Enter your age")
+// document.write(typeof(age))
+
+// let age="23"
+
+
+
+// document.write(isNaN(age))
+
+// confirm("Are you sure?")
+
+
+
